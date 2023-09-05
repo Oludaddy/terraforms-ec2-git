126a4ba98b21c3d8276dc8758d61ab1bdf2c3145
Olufemi <olufemifamiloyo@gmail.com>
2023-09-05 11:21:03 -0400

    configure credentials for terraform cloud

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "web" {
-  ami           =  "ami-051f7e7f6c2f40dc1"   
+  ami           = "ami-051f7e7f6c2f40dc1"
   instance_type = "t2.micro"
 
   tags = {
@@ -8,7 +8,7 @@ resource "aws_instance" "web" {
 }
 
 resource "aws_instance" "web-1" {
-  ami           =  "ami-051f7e7f6c2f40dc1"   
+  ami           = "ami-051f7e7f6c2f40dc1"
   instance_type = "t2.micro"
 
   tags = {
@@ -21,18 +21,17 @@ resource "aws_instance" "web-1" {
 terraform {
   required_providers {
     aws = {
-      source = "hashicorp/aws"
+      source  = "hashicorp/aws"
       version = "5.15.0"
     }
   }
 }
 
 provider "aws" {
-  region      = var.AWS_REGION   
-  access_key  = var.AWS_ACCESS_KEY_ID
-  secret_key  = var.AWS_SECRET_ACCESS_KEY
-} 
-
+  region     = var.AWS_REGION
+  access_key = var.AWS_ACCESS_KEY_ID
+  secret_key = var.AWS_SECRET_ACCESS_KEY
+}
 variable "AWS_REGION" {}
 
 
